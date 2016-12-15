46787db6c964d49280410e85053ce114d662625e
Daniel Enache <daniel@bakemono.ro>
2016-12-15 22:55:25 +0200

    added generic add and edit pages

--- gong/web/config.py
+++ gong/web/config.py
@@ -3,6 +3,5 @@ import os
 from jinja2 import Environment, FileSystemLoader
 
 
-template_path = os.path.join(os.path.dirname(__file__),'templates')
-print template_path
+template_path = os.path.join(os.path.dirname(__file__), 'templates')
 jinja = Environment(loader=FileSystemLoader(template_path))
